6a43e44b2ff051698d7fbd7e0d4e86bc1a8925be
Thomas Roten <thomas@roten.us>
2015-12-14 05:33:26 -0600

    Adds sample test case.

---
+++ tp/tests/test_tp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Unit tests for the `tp` module."""
+
+import unittest
+
+
+class TestTp(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
